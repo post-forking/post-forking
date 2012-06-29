09d05da922f742a9afaa3052b4377868ed3d585e
Benjamin J. Balter <ben@balter.com>
2012-06-29 18:38:16 -0500

    adding conflict warning

---
+++ templates/conflict-warning.php
@@ -0,0 +1,1 @@
+<div class="error" id="conflict-warning"><p><?php _e( 'This fork cannot be merged with its parent post. Please resolve the conflict below before attempting to publish again.', 'fork' ); ?></p></div>
