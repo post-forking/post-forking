86e23cf26223893eaac8880255a1a137c28a41ef
Daniel Bachhuber <d@danielbachhuber.com>
2012-09-07 08:44:28 -0700

    Until custom capabilities are properly implemented, use the normal capabilities
    See #10

--- fork.php
+++ fork.php
@@ -124,8 +124,8 @@ class Fork {
 			'query_var'		   => true,
 			'can_export'		  => true,
 			'rewrite'			 => true,
-			'map_meta_cap'		=> true,
-			'capability_type'	 => 'fork',
+			// 'map_meta_cap'		=> true, // Neds to be properly implemented, see https://github.com/benbalter/post-forking/issues/10
+			// 'capability_type'	 => 'fork',
 		);
 	
 		register_post_type( 'fork', $args );
