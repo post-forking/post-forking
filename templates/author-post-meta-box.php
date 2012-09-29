7179850fbea742e2d7baca79317340a25d2c2fdf
Aaron Jorbin <aaron@jorb.in>
2012-09-28 23:04:01 -0400

    Make the author meta box a bit prettier

--- templates/author-post-meta-box.php
+++ templates/author-post-meta-box.php
@@ -1,1 +1,1 @@
-<a href="<?php echo admin_url( "?fork={$post->ID}" ); ?>" class="button button-primary"><?php _e( 'Create new Branch', 'fork' ); ?></a> 
+<div class="fork-actions">    <a href="<?php echo admin_url( "?fork={$post->ID}" ); ?>" class="button button-primary create-new-fork-button"><?php _e( 'Create New Branch', 'fork' ); ?></a>     <div class="clear"></div></div>
--- templates/branches-dropdown.php
+++ templates/branches-dropdown.php
@@ -1,6 +1,9 @@
-<?php _e( 'Branch:', 'fork' ); ?> <select name="branches" id="branches" class="branches">
+<div class="fork-select">
+<label for="branches"><?php _e( 'Branch:', 'fork' ); ?></label> <select name="branches" id="branches" class="branches">
 	<option id="original" value="<?php echo esc_attr( $post->ID ); ?>" class="original"><?php _e( 'Original', 'original' ); ?></option>
 <?php foreach ( $branches as $branch ) { ?>
 	<option id="<?php echo esc_attr( $branch->post_name ); ?>" value="<?php echo esc_attr( $branch->ID ); ?>" <?php selected( $post->ID, $branch->ID ); ?>><?php echo $branch->post_title; ?></option>
 <?php } ?>
 </select>
+<div class="clear"></div>
+</div>
