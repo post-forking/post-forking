a652ee2576aa5f39a7e352361422f5ccc3f35133
Daniel Bachhuber <d@danielbachhuber.com>
2013-04-15 16:51:50 -0700

    Tests: Bootstrap file moved in the WP Unit test checkout
    See #54

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -16,6 +16,6 @@ $GLOBALS['wp_tests_options'] = array(
 	'active_plugins' => array( 'post-forking/post-forking.php' ),
 );
 
-require getenv( 'WP_TESTS_DIR' ) . '/bootstrap.php';
+require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
 require dirname( __FILE__ ) . '/wp_die_handler.php';
 require dirname( __FILE__ ) . '/post_forking_test.php';
